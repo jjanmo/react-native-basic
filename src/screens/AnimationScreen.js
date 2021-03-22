db432617770a6d8f5f3bb5732b021f517281dd48
jjanmo <jjanmo07@hanmail.net>
2021-03-23 01:11:42 +0900

    Add fearture about translated box in react native

--- src/screens/AnimationScreen.js
+++ src/screens/AnimationScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
+import Move from '../animations/Move';
 import HomeButton from '../components/HomeButton';
 
 const AnimationScreen = ({navigation}) => {
   return (
-    <>
-      <Text>Animation Screen</Text>
+    <View style={styles.container}>
+      <Move />
       <HomeButton navigator={navigation.navigate} />
-    </>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default AnimationScreen;
