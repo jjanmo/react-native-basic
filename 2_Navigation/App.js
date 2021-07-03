4862048adba4e0ca3da4c6ff2723c9dad2776844
jjanmo <jjanmo07@hanmail.net>
2021-07-03 22:06:25 +0900

    feat: setup react navigation

---
+++ 2_Navigation/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import 'react-native-gesture-handler';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
