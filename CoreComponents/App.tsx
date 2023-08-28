de6f830bf249bdf4951b96c58248d541da2ef565
jjanmo <jjanmo07@hanmail.net>
2023-08-28 16:02:27 +0900

    feat: add project and configure setup

---
+++ CoreComponents/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
